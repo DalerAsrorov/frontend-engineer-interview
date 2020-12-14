ec2e4176cc304d87adcbb1c59acc5c14533ca473
DalerAsrorov <asrorids@gmail.com>
2020-12-13 21:53:27 -0500

    linkedin question

--- predict-output/linkedin.js
+++ predict-output/linkedin.js
@@ -1,3 +1,8 @@
+/**
+ * A
+ */
+
+console.log('A');
 var X = function (someValue) {
     this.hello = function () {
         return someValue;
@@ -14,3 +19,33 @@ var x = new X("hi");
 // ------what will the following code return?
 console.log(x.hello()); // "hi"
 console.log(x.test()); // undefined
+
+
+/**
+ * B
+ */
+
+// What does this code return?
+
+console.log('B');
+var Foo = function (a) {
+    this.bar = () => {
+        return a;
+    };
+
+    var baz = function () {
+        return a;
+    };
+};
+
+Foo.prototype = {
+    biz: () => {
+        return this.bar();
+    }
+};
+
+var f = new Foo(7);
+f.bar(); //=> 7
+f.baz(); //=> Error: f.baz is not a function - since it's a private function
+f.biz(); //=> Error: this.bar is not a function - will not find the method
+// because it uses the arrow function which refers to the parent scope (global scope)
